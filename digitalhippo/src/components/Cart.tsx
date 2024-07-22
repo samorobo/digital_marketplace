d7a64fa645017913e5ba55241c95b8dfec8b2242
samorobo <samgodwin111@gmail.com>
2024-07-22 06:20:50 +0100

    My overall commit

--- digitalhippo/src/components/Cart.tsx
+++ digitalhippo/src/components/Cart.tsx
@@ -6,9 +6,10 @@ import { ShoppingCart } from "lucide-react"
 import { formatPrice } from "@/lib/utils"
 import Link from "next/link"
 import { Button, buttonVariants } from "./ui/button"
+import Image from "next/image"
 
 const Cart = () => {
-    const itemCount = 1
+    const itemCount = 0
     const fee  = 1
     return (
         <Sheet>
@@ -51,7 +52,21 @@ const Cart = () => {
                         </SheetFooter>
                     </div>
                 ) : (
-                    <div></div>
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div aria-hidden="true" className="relative mb-4 h-60 w-60 text-muted-foreground">
+                            <Image src='/hippo-empty-cart.png' alt="empty shopping cart hippo" fill />
+                        </div>
+                        <div className="text-xl font-semibold">Your cart is empty</div>
+                        <SheetTrigger>
+                            <Link href='/products' className={buttonVariants({
+                                variant: 'link',
+                                size: "sm",
+                                className: 'text-sm text-muted-foreground' 
+                            })}>
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
                 ) }
             </SheetContent>
         </Sheet>
